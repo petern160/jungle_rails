3c9e6af66a1782b3693d0f3b39d38b2836df95d9
petern160 <peternguyen160@gmail.com>
2019-05-15 20:39:52 +0000

    displayed all categories under category page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products,  except: [:edit, :update, :show]
+    resources :categories, only: [:create, :new, :index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
