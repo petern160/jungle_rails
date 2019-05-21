93a3034776472907402511fd42feac0b96174cea
petern160 <peternguyen160@gmail.com>
2019-05-21 19:13:54 +0000

    finished testing for prodcucts with capybara img

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "capybara/rails"
-require "capybara/rspec"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
 require "capybara/poltergeist"
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+
 
 
 Capybara.javascript_driver = :poltergeist
@@ -26,7 +27,7 @@ Capybara.javascript_driver = :poltergeist
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
